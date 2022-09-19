7107d5545dc2e3b442ecaff8bb6f8ef1f936abdb
Pawel Smolarski <smolenc1@gmail.com>
2022-09-19 20:00:59 +0200

    appBooks part5

--- src/js/script.js
+++ src/js/script.js
@@ -8,12 +8,20 @@
     containerOf: {
       listOfBooks:'.books-list',
       imageBooks: '.book__image'
+    },
+    filterOf: {
+      filterBook: '.filters input'
+
     }
   };
   const templates = {
     listBooks: Handlebars.compile(document.querySelector(select.templateOf.menuBooks).innerHTML),
   };
-
+  const favoriteBooks = [];
+  const filters = [];
+  const bookListProduct = document.querySelector(select.containerOf.listOfBooks);
+  const filterInput = document.querySelectorAll(select.filterOf.filterBook);
+  // const booksImage = document.querySelector(select.containerOf.imageBooks);
 
   const render = () => {
     for(let elem of dataSource.books ){
@@ -24,44 +32,65 @@
       booksMenu.append(generatedDOM);
     } 
   };
-  render();
-
-
-  const favoriteBooks = [];
+  
   const initActions = () => {
-    const bookListProduct = document.querySelector(select.containerOf.listOfBooks);
-    
-    
-      
     bookListProduct.addEventListener('dblclick', function(event){
-      console.log('click');
       event.preventDefault();
       const clicked = event.target;
-      console.log(clicked.offsetParent);
-      const bookId = clicked.offsetParent.getAttribute('data-id');
+      const bookId = clicked.offsetParent.getAttribute('data-id'); 
       if(clicked.offsetParent.classList.contains('book__image')) {
-        
         if(!clicked.offsetParent.classList.contains('favorite')) {
           clicked.offsetParent.classList.add('favorite');
           favoriteBooks.push(bookId);
-          console.log(favoriteBooks);
-        } else {
+        }else{
           clicked.offsetParent.classList.remove('favorite');
           const toRemove = favoriteBooks.indexOf(bookId);
           favoriteBooks.splice(toRemove, 1);
-          console.log(favoriteBooks);
-
         }
       }
     });
-         
+    for(let i = 0; i < filterInput.length; i++ ){
+      filterInput[i].addEventListener('click', function(event){
+        const clicked = event.target;
+        if(clicked.type == 'checkbox' && clicked.tagName == 'INPUT' && clicked.name == 'filter'){
+          if(clicked.checked){
+            filters.push(clicked.value);
+            
+          }else{
+            const toRemove = filters.indexOf(clicked.value);
+            filters.splice(toRemove, 1);
+            
+          }
+        }
+        filterBooks();
+        for(let input of filterInput){
+          input.addEventListener('change', filterBooks);
+        }
+      });
+    }
+  }; 
+  const filterBooks = () => {
+    for(const elem of dataSource.books) {
+      let shouldBeHidden = false;
+      for(const filter of filters){
+        if(!elem.details[filter]) {
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      if(shouldBeHidden){
+        document.querySelector(`[data-id="${elem.id}"]`).classList.add('hidden');
+      } else {
+        document.querySelector(`[data-id="${elem.id}"]`).classList.remove('hidden');
+      }
+      
+    }  
   };
-     
-  
-  initActions();
-  
-
+  render();
+  initActions();     
 }
+
+
 
 
 
